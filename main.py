d9b604f4d609bebef4c6f2d4d322afdec80f55d9
rjtelford <rjtelford@gmail.com>
2025-04-14 18:21:34 -0600

    Created groups

--- main.py
+++ main.py
@@ -9,16 +9,25 @@ def main():
     pygame.init()
     dt = 0
     delta_clock = pygame.time.Clock()
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    
+    player.Player.containers = (updatable, drawable)
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     player_ship = player.Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
-        player_ship.draw(screen)
+
+        for item in drawable:
+            item.draw(screen)
+        
+        updatable.update(dt)
 
         pygame.display.flip()
         dt = delta_clock.tick(60) / 1000
